daeaaf90bd7746f306141c091b024fc634a0004b
Biswayan Banerjee <89703859+BiswayanBanerjee@users.noreply.github.com>
2024-12-29 02:09:52 +0530

    Update dbMySQL.ts

--- backend/src/config/dbMySQL.ts
+++ backend/src/config/dbMySQL.ts
@@ -14,14 +14,16 @@
 import mysql from "mysql2/promise";
 
 // Directly include MySQL connection details
-const MYSQL_HOST = "localhost"; // Your MySQL host
-const MYSQL_USER = "root"; // Your MySQL username
-const MYSQL_PASSWORD = "Nayan29@Account"; // Your MySQL password
-const MYSQL_DATABASE = "authdb"; // Your MySQL database name
+const MYSQL_HOST = "sql12.freesqldatabase.com"; // Your MySQL host
+const MYSQL_USER = "sql12754609"; // Your MySQL username
+const MYSQL_PASSWORD = "shKzpe2TVS"; // Your MySQL password
+const MYSQL_DATABASE = "sql12754609"; // Your MySQL database name
+const MYSQL_PORT=3306;
 
 export const mysqlPool = mysql.createPool({
   host: MYSQL_HOST,
   user: MYSQL_USER,
   password: MYSQL_PASSWORD,
   database: MYSQL_DATABASE,
+  port:MYSQL_PORT;
 });
